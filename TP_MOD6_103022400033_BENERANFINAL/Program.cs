570fcbb6c236a660eecb7ed08000642b81cce301
Annisa Azzahra Putri <annisaazzahraputrizahra@gmail.com>
2026-04-05 21:34:36 +0700

    menambahkan class SayaMusicTrack

--- TP_MOD6_103022400033_BENERANFINAL/Program.cs
+++ TP_MOD6_103022400033_BENERANFINAL/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MOD6_103022400033_BENERANFINAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaMusicTrack track = new SayaMusicTrack("Taylor Swift – exile feat. Bon Iver");
+
+            track.PrintTrackDetails();
+
+            track.IncreasePlayCount(100);
+            track.PrintTrackDetails();
+
+            track.IncreasePlayCount(50);
+            track.PrintTrackDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TP_MOD6_103022400033_BENERANFINAL/SayaMusicTrack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics; // untuk Debug.Assert
+
+namespace TP_MOD6_103022400033_BENERANFINAL
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+            this.playCount = 0;
+            this.title = title;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("=== SayaMusicTrack Details ===");
+            Console.WriteLine($"ID         : {id}");
+            Console.WriteLine($"Title      : {title}");
+            Console.WriteLine($"Play Count : {playCount}");
+            Console.WriteLine("===============================\n");
+        }
+    }
+}
